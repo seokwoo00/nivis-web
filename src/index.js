6c501c63ba036dd7aeea44e5231d105df9bb2627
yotyot <82359786+yot-yot@users.noreply.github.com>
2025-10-17 14:55:10 +0900

    gpt god

--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@ const server = app.listen(80, ()=>{
     console.log("server start!");
 })
 
+app.use(express.static('src'));
+
 app.get('/', async (req, res) => { 
     var filePath = path.join(__dirname, 'html', 'main.html');
+    res.sendFile(filePath);
+})
+
+app.get('/write', async (req, res) => { 
+    var filePath = path.join(__dirname, 'html', 'write.html');
+    res.sendFile(filePath);
+})
+
+app.get('/post', async (req, res) => { 
+    var filePath = path.join(__dirname, 'html', 'post.html');
     res.sendFile(filePath);
 })
